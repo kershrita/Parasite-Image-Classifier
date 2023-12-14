fd128c7329b49f5316599aba7d98cb161dcaa0ab
Ashraf Elkersh <ashrafabdulkhaliq80@gmail.com>
2023-12-14 22:54:54 +0200

    delete img equalization notebook & make a func to do the same thing

--- Main.py
+++ Main.py
@@ -1,5 +1,9 @@
 import os
+import shutil
+from glob import glob
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
 def split_images_by_shape(input_folder, output_folder_low, output_folder_high, threshold_width, threshold_height):
     # Iterate through each class folder
@@ -32,3 +36,19 @@ def split_images_by_shape(input_folder, output_folder_low, output_folder_high, t
             # Save the image to the appropriate output folder
             cv2.imwrite(output_path, image)
         print("Done!")
+
+
+def image_equalization(img):
+    # Calculate the cumulative distribution function (CDF)
+    cdf = hist.cumsum()
+    cdf_normalized = cdf * hist.max() / cdf.max()
+
+    # Create a dictionary for mapping the original pixel values to the equalized values
+    cdf_m = np.ma.masked_equal(cdf, 0)
+    cdf_m = (cdf_m - cdf_m.min()) * 255 / (cdf_m.max() - cdf_m.min())
+    cdf = np.ma.filled(cdf_m, 0).astype('uint8')
+    
+    # Apply the equalization to the image using the CDF mapping
+    equalized_image = cdf[image]
+
+    return equalized_image
